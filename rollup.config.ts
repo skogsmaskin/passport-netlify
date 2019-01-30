1497ebd8a31fd33a9a97ebc3eafc0857ae50f9f6
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2019-01-30 09:09:10 +0100

    Maintainance update (0.0.4)

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,8 +12,7 @@ const libraryName = 'passport-netlify'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.main, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
